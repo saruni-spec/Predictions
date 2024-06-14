51f75d3998ae7cdd7a56e09f1f9f3bd6be33973a
boss <smithsaruni16@gmail.com>
2024-06-14 14:32:31 +0300

    localstorage

--- scripts/drag_list.js
+++ scripts/drag_list.js
@@ -3,9 +3,22 @@ $(function () {
   $(".sortable").disableSelection();
 });
 
-function checkOrder() {
-  let order = [];
-  $(".sortable li").each(function (index) {
-    order.push($(this).text());
+function storePredictions() {
+  let predictions = {};
+
+  $(".groups").each(function () {
+    let groupName = $(this).find("h3").text().trim().split(" ")[1]; // Get the group letter
+    let order = [];
+
+    $(this)
+      .find(".sortable li")
+      .each(function () {
+        order.push($(this).text().trim());
+      });
+
+    predictions[groupName] = order;
   });
+
+  console.log(predictions); // For debugging purposes
+  localStorage.setItem("predictions", JSON.stringify(predictions));
 }
---
+++ scripts/final_predictions.js
@@ -0,0 +1,5 @@
+let predictions = JSON.parse(localStorage.getItem("predictions"));
+
+window.onload = function getPredictions() {
+  console.log(predictions);
+};
